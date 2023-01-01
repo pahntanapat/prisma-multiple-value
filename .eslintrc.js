750197e7224260f3e33b6a0538c2bf170707627b
Tanapat Kahabodeekanokkul <pahntanapat@gmail.com>
2023-01-01 12:08:17 +0700

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "quotes": ["error", "double"],
+        "@typescript-eslint/no-explicit-any": "off",
+        "no-useless-catch": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
+
+    }
+}
